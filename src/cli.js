d699a998abb505ab0530d70822f80c5aeee67369
Daniel Kentfield <dan@mrdan.dev>
2021-12-01 07:56:51 +0000

    Add better instructions

--- src/cli.js
+++ src/cli.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 });
 
 rl.question("What size grid do you want to use? " +
-  "\n hint: use the format (x, y) e.g. 4, 8 \n", function(grid) {
+  "\n hint: use the format x y with no commas e.g. 4 8 \n", function(grid) {
   rl.question("Add a robot in the format (x, y, ORIENTATION) instructions..." +
     "\n e.g. (2, 3, N) FFRLF \n", function(robot) {
     console.log(`You built a grid of ${grid}, and a robot of ${robot}`);
